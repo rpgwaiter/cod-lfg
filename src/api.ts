7f2708d0086dcccfcb2bb83e8bb2f6cce3b22967
rpgwaiter <rpgwaiter@based.zone>
2024-07-24 03:56:52 -0600

    some validation for atvi ids, reworking the api a bit

--- src/api.ts
+++ src/api.ts
@@ -1,31 +1,41 @@
 import { formatTimeAgo } from './lib.ts'
 import type { KVNamespace } from '@cloudflare/workers-types'
 
-export async function apiHandler (request: Request, env) {
+export async function apiHandler(request: Request, env) {
   const KV: KVNamespace = env.CODSOCIAL
   const u = new URL(request.url)
 
   if (u.pathname === '/api/submit') {
     const s = new URLSearchParams(u.search)
-    const message = s.get('message')
     const now = new Date().getTime() / 1000
-    const p = {
-      atvid: s.get('atvid'),
-      rank: s.get('rank'),
-      platform: s.get('platform'),
-      hasMic: !!s.get('hasMic'),
-      noLife: !!s.get('noLife'),
-      party: s.get('party'),
-      ttl: Number(s.get('ttl') || 60),
-      isMuted: !!s.get('isMuted'),
-      posted: now,
-      message: message && encodeURIComponent(message)
+
+    const atvid = s.get('atvid')
+    const rank = s.get('rank')
+    const platform = s.get('platform')
+    const hasMic = !!s.get('hasMic')
+    const noLife = !!s.get('noLife')
+    const party = s.get('party')
+    const ttl = Number(s.get('ttl') || 60)
+    const isMuted = !!s.get('isMuted')
+    const posted = now
+    const message = s.get('message')?.substring(0, 250) || ''
+
+    console.log(atvid)
+
+    // check for valid atvi ids
+    if (!atvid || !/[A-Za-z0-9\ ]{2,30}\#[0-9]{2,20}/.test(atvid)) {
+      return new Response('', {
+        status: 302,
+        headers: { Location: `/?error="Invalid Activision ID."` }
+      })
     }
 
+    const player = { atvid, rank, platform, hasMic, noLife, party, ttl, isMuted, message, posted }
+
     // TODO: dupe check
-    if (p.atvid) {
+    if (player.atvid) {
       console.log('putting into KV')
-      await KV.put(`post:${p.atvid}`, JSON.stringify(p), { expirationTtl: p.ttl * 60 })
+      await KV.put(`post:${player.atvid}`, JSON.stringify(player), { expirationTtl: player.ttl * 60 })
     }
 
     return new Response('', {
